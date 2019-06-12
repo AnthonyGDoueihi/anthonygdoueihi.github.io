fc7244aed08c4c556e569a09ed155f21f26179b4
Anthony <agdoueihi@gmail.com>
2019-06-13 08:20:46 +1000

    Initial Commit

---
+++ js/events.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const works = document.querySelectorAll(".workContainer");
+
+  works.forEach( (work) => {
+    work.addEventListener("click", (event) => {
+
+      const front = work.getElementsByClassName('workFront')[0];
+      const back = work.getElementsByClassName('workBack')[0];
+
+        if( front.className === "work workFront workVisible"){
+
+          front.className = "work workFront workHidden";
+          back.className = "work workBack workVisible";
+
+        }else{
+
+          front.className = "work workFront workVisible";
+          back.className = "work workBack workHidden";
+
+        }
+
+    })
+  })
+});
---
+++ js/mouseRecord.js
@@ -0,0 +1,14 @@
+const mouseMove = [];
+
+const getPos = function(e){
+  mouseMove.push()
+}
+
+document.onmousemove = function(e){
+  console.log(window);
+  // window.innerWidth Need to get the page width and height to get the actual relative
+  // e.pageX
+  // window.innerHeight
+  // e.pageY
+
+}
